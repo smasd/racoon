cf54cb86fe4e0344f63675334a560236205b1a64
Sam <xksmitty@hotmail.com>
2021-06-03 18:33:41 +1000

    finish basic functionality of Discord

--- lib/Discord.py
+++ lib/Discord.py
@@ -1,5 +1,6 @@
 import discord
 import lib.Settings as s
+from time import sleep
 
 config = s.Settings('discord.json')
 
@@ -7,11 +8,42 @@ config = s.Settings('discord.json')
 class Discord:
     def __init__(self):
         self.token = config.get('token')
-        self.channel = config.get('channel')
+        self.channel = int(config.get('channel'))
+        self.sleep_time = config.get("sleep_time")
 
         self.client = discord.Client()
 
-        self.on_ready = self.client.event(self.on_ready)
-        self.on_message = self.client.event(self.on_message)
+        self.messages = config.get("message_list")
 
+        @self.client.event
+        async def on_ready():
+            while True:
+                for payload in self.messages:
+                    message = payload["message"]
+                    attachment = payload["attachment"]
 
+                    if attachment is not None:
+                        await self.client.get_channel(self.channel).send(message, file=discord.File(attachment))
+                    else:
+                        await self.client.get_channel(self.channel).send(message)
+                    self.messages.remove(payload)
+                    self.save_messages()
+                    break
+                if not self.messages:
+                    sleep(self.sleep_time)
+
+        @self.client.event
+        async def on_message(message):
+            if not message.author == self.client.user:
+                print("I got a message")
+
+    def run(self):
+        self.client.run(self.token, bot=True)
+
+    def add_message(self, message, attachment=None):
+        content = {"message": message, "attachment": attachment}
+        self.messages.append(content)
+        self.save_messages()
+
+    def save_messages(self):
+        config.set("message_list", self.messages)
--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import lib.Discord as d
+from pathlib import Path
+from time import sleep
 import threading
 
 discord = d.Discord()
-app = d.TDCRBot()
 
-thread = threading.Thread(target=app.run)
+
+def add_test_message():
+    attachment = "meme.jpg"
+    discord.add_message("lol", attachment)
 
 
 def main():
+    thread = threading.Thread(target=add_test_message)
     thread.start()
-    #print("carry on")
+    discord.run()
 
 
 if __name__ == "__main__":
